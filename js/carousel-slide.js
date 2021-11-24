385e00d8235f8f759877c49aa9d47d39e3bb5985
koyuki saito <koyukisaito@koyukinoMacBook-Pro.local>
2021-11-24 15:29:30 +0900

    11/24-13

--- js/carousel-slide.js
+++ js/carousel-slide.js
@@ -2,6 +2,7 @@ $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: false,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
+    speed: 200,
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
